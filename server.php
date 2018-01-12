c235f930cc328441b8eca76334fe5fb7353b376e
wilmar <wil.pac510@gmail.com>
2018-01-12 12:10:26 -0600

    cargando el servicio web de operaciones basicas

---
+++ server.php
@@ -0,0 +1,45 @@
+<?php
+// Servicio para operaciones basicas
+class Operacion {
+
+    /**
+     * Suma dos operadores
+     * @param integer 
+     * @return integer
+     */
+    public function add($a, $b) {
+        return $this->_add($a, $b);
+    }
+
+    /**
+     * Resta
+     * @param integer 
+     * @return integer
+     */
+    public function sub($a, $b) {
+        return $a - $b;
+    }
+
+    /**
+     * Multiplicación
+     * @param integer 
+     * @return integer
+     */
+    public function mul($a, $b) {
+        return $a * $b;
+    }
+
+    /**
+     * Método protegido, no está espuesto
+     * @param integer 
+     * @return integer
+     */
+    protected function _add($a, $b) {
+        return $a + $b;
+    }
+}
+
+$server = new Yar_Server(new Operacion());
+$server->handle();
+
+?>
